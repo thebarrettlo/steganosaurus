d702fffb0d2f896dfc81f1123cb2973b3fc1e5a5
Barrett Lo <beebearlo@gmail.com>
2019-05-24 11:29:35 +0300

    Functional. References Steganoseed.py to generate random seed and alphabet shift. Currently only creates a noise image

--- steganosaurus.py
+++ steganosaurus.py
@@ -1,28 +1,34 @@
 # STEGANOSAURUS by Barrett Lo
 # Begun on May 22, 2019
 #
+# Steganosaurus encodes a message into an image file.
+#
 
 from PIL import Image, ImageDraw
+import random
+import steganoseed
 
-def encodeText(input_text):
+def encodeText(input_text, savekey):
     """Encodes user's text into an image file. Takes user's text and image to cover up the coded image."""
-
+    
     # Translate user input to ASCII values stored in a list
     ASCIIinput = []
     for char in input_text:
         ASCIIinput.append(ord(char))
+    key = steganoseed.generate_key(savekey)
+    steganoseed.ASCIIshift(ASCIIinput, key)
+    random.seed(steganoseed.generate_key(key))   # User-generated save key for decoding
+    random.shuffle(ASCIIinput)
 
     # Initialize blank image for encoding
-    width = 300
-    height = 500
+    width = 400
+    height = 200
     template = Image.new("RGBA", (width, height))
-    splitted = template.split() # Split into R, G, B, A channels
 
     # Change pixel colors
     draw = ImageDraw.Draw(template)
     x = 0
     y = 0
-
     i = 0
     while i < (len(ASCIIinput) - (len(ASCIIinput) % 3)):
         draw.point([(x, y)], (ASCIIinput[i], ASCIIinput[i+1], ASCIIinput[i+2]))
@@ -38,5 +44,36 @@ def encodeText(input_text):
         draw.point([(x, y)], (ASCIIinput[i], 0, 0))
     elif (len(ASCIIinput) % 3) == 2:
         draw.point([(x, y)], (ASCIIinput[i], ASCIIinput[i+1], 0))
+    
+    template.save("exportImage.png")
 
-    template.save("encodedImage.png")
+
+def decodeText(input_image, savekey):
+    """Decodes text contained in image that was encoded using Steganosaurus."""
+    
+    img = Image.open(input_image)
+    coded_text = []
+
+    # Pull the encoded pixels
+    for i in list(img.getdata()):
+        if i[3] == 255:
+            coded_text.append(i[0])
+        if i[1] != 0:
+            coded_text.append(i[1])
+        if i[2] != 0:
+            coded_text.append(i[2])
+
+    # Decode using save key
+    temp = range(len(coded_text))
+    key = steganoseed.generate_key(savekey)
+    steganoseed.deASCIIshift(coded_text, key)
+    random.seed(steganoseed.generate_key(key))
+    random.shuffle(temp)
+    decoded_text = [None] * len(coded_text)
+    for i, x in enumerate(temp):
+        decoded_text[x] = chr(coded_text[i])
+    
+    return "".join(decoded_text)
+
+
+#encodeText("Lorem ipsum dolor sit amet, amet accusam sea in, justo tation ut mel? Nostro utamur te nam, ex omnesque oportere nam. Vix ut summo audire consequuntur! Solet qualisque in sit! In vim posse forensibus, ut usu clita facete. Mel an omnis prima interesset, vel alii abhorreant et, et est reque paulo fastidii! No mel choro petentium vituperatoribus. Nec ut graece abhorreant. Ex ius modus homero honestatis, ne pri enim saepe oportere. Omnis perfecto mei id. Consul vituperata per eu? At dicam dolorem inciderint eum. Ut eos summo tamquam, qui no affert fuisset mnesarchum, pro id quot blandit! Rationibus quaerendum his ut, nostrud reformidans ullamcorper sea in, utinam fastidii reprimique ad nec.", "1d3#J%")
---
+++ steganoseed.py
@@ -0,0 +1,48 @@
+# Steganoseed generates int for random.seed() from a given save key.
+# Barrett Lo
+# 24 May, 2019
+#
+
+import math
+
+def generate_key(savekey):
+    """Generates a key from the given save key. The save key must be between five and twenty characters, and may include
+    uppercase, lowercase, numbers, and special characters."""
+
+    # Convert save key to integer values
+    keyarray = list(str(savekey))
+    i = 0
+    for char in keyarray:
+        keyarray[i] = ord(char)
+        i += 1
+
+    # Mathematically manipulate save key values
+    keyval = int(((((keyarray[0] % keyarray[1]) % sum(keyarray[3:-1])) * math.exp(keyarray[2])) // keyarray[-1]))
+
+    return keyval
+
+
+def ASCIIshift(ascii_array, savekey):
+    """ASCIIshift shifts all ASCII values in an array by an amount determined by the save key."""
+
+    key = sum([int(i) for i in list(str(generate_key(savekey)))])
+
+    i = 0
+    while i < len(ascii_array):
+        ascii_array[i] = (ascii_array[i] + key) % 255
+        i += 1
+
+    return ascii_array
+
+
+def deASCIIshift(ascii_array, savekey):
+    """ASCIIshift un-shifts all ASCII values in an array by an amount determined by the save key."""
+
+    key = sum([int(i) for i in list(str(generate_key(savekey)))])
+
+    i = 0
+    while i < len(ascii_array):
+        ascii_array[i] = (ascii_array[i] - key)
+        i += 1
+
+    return ascii_array
